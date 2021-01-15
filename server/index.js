49e1ed11f4909a9c7872b81ebcada73598e9701f
Dwayne <deandraper82@gmail.com>
2021-01-14 22:00:04 -0600

    Server and React LAT-28

---
+++ server/index.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+const port = 3001;
+
+app.use(express.static(__dirname + '/../client/dist'));
+
+app.get('/', (req, res) => {
+  //code
+});
+
+app.listen(port, () => {
+  console.log(`Listening on ${port}`);
+});
