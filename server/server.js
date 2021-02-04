2d3aa5535f4f9b8ec55d9c9f2785b4d92682f062
Dwayne <deandraper82@gmail.com>
2021-02-04 16:54:14 -0600

    Cleaned up styles.js file after cherry pick LAT-89

--- server/server.js
+++ server/server.js
@@ -34,22 +34,6 @@ app.get('/metadata/', (req, res) => {
   });
 });
 
-app.get('/metadata/:id', (req, res) => {
-  console.log('reqbody', req.body);
-  console.log('reqparams', req.params);
-  console.log(req.query);
-  db.getGame(req.params.id, (err, data) => {
-    if (err) {
-      console.error('Failed data retrieval', err);
-      res.sendStatus(500);
-    } else {
-      const splitData = utils.splitData(data);
-      console.log('app.get', splitData);
-      res.send(splitData);
-    }
-  });
-});
-
 app.listen(port, () => {
   console.log(`Listening on ${port}`);
 });
